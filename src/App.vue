266e0d5cded3f725874726c0389cc4c71e71d535
SergioCruzSalinas <sergiocruz7913@gamil.com>
2024-05-02 14:03:50 -0600

    cambios en el filtro

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-   <h1 class="titulo">Pokémon</h1>
-</div>
+    <h1 class="titulo">Pokémon</h1>
+  </div>
 
- <div>
-   <PokemonFilter @update-search-term="handleUpdateSearchTerm" />
- </div>
- <div>
-   <PokemonList :pokemons="pokemons" :filteredPokemons="pokemonsFilters" />
- </div>
+  <div>
+    <PokemonFilter @update-search-term="handleUpdateSearchTerm" />
+  </div>
 
+  <div>
+    <PokemonList :pokemons="pokemons" :filteredPokemons="pokemonsFilters" />
+  </div>
 </template>
 
 <script>
@@ -18,75 +18,72 @@ import PokemonList from '/src/components/PokemonList.vue';
 import axios from 'axios';
 
 export default {
- components: {
-   PokemonList,
-   PokemonFilter
- },
- data() {
-   return {
-     pokemons: [],
-     searchTerm:'',
-     pokemonsFilters: []
-   };
- },
- computed: {
- },
- created() {
-   this.fetchAllPokemons();
- },
- methods: {
-   filteredPokemons(filterName) {
-     if ( !filterName ) {
-       this.pokemonsFilters = [...this.pokemons]
-       return
-     } 
-     // console.log(filterName, 'desde filter function');
-     filterName = filterName.toLowerCase();
+  components: {
+    PokemonList,
+    PokemonFilter
+  },
+  data() {
+    return {
+      pokemons: [],
+      searchTerm:'',
+      pokemonsFilters: []
+    };
+  },
+  created() {
+    this.fetchAllPokemons();
+  },
+  methods: {
+    filteredPokemons(filterTerm) {
+      if (!filterTerm) {
+        this.pokemonsFilters = [...this.pokemons];
+        return;
+      }
 
-     let pokemonsWithFilter = this.pokemons.filter( pokemon => pokemon.name.toLowerCase().includes(filterName))
+      filterTerm = filterTerm.toLowerCase();
 
-     this.pokemonsFilters = [...pokemonsWithFilter]
+      let pokemonsWithFilter = this.pokemons.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(filterTerm) ||
+        parseInt(filterTerm) === pokemon.id
+      );
 
-   },
-   handleUpdateSearchTerm(filterName) {
-     // console.log(algo);
-     // console.log('padre recibiendo el emit')
-     this.filteredPokemons(filterName);
-   },
-   async fetchAllPokemons() {
-     try {
-       const response = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=811');
-       const pokemonList = response.data.results;
-       await Promise.all(
-         pokemonList.map(async (pokemon) => {
-           const pokemonResponse = await axios.get(pokemon.url);
-           this.pokemons.push({
-             name: this.capitalizarPrimeraLetra(pokemonResponse.data.name),
-             id: pokemonResponse.data.id,
-             types: pokemonResponse.data.types.map((typeInfo) => typeInfo.type.name),
-             img: pokemonResponse.data.sprites.front_default,
-           });
-         })
-       );
-       this.pokemons.sort((a, b) => a.id - b.id);
-       this.pokemonsFilters = [...this.pokemons]
-     } catch (error) {
-       console.error('Error fetching Pokémon data:', error);
-     }
-   },
-   capitalizarPrimeraLetra(string) {
-     return string.charAt(0).toUpperCase() + string.slice(1);
-   },
- },
+      this.pokemonsFilters = [...pokemonsWithFilter];
+    },
+    handleUpdateSearchTerm(filterTerm) {
+      this.filteredPokemons(filterTerm);
+    },
+    async fetchAllPokemons() {
+      try {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=811');
+        const pokemonList = response.data.results;
+        await Promise.all(
+          pokemonList.map(async (pokemon) => {
+            const pokemonResponse = await axios.get(pokemon.url);
+            this.pokemons.push({
+              name: this.capitalizarPrimeraLetra(pokemonResponse.data.name),
+              id: pokemonResponse.data.id,
+              types: pokemonResponse.data.types.map((typeInfo) => typeInfo.type.name),
+              img: pokemonResponse.data.sprites.front_default,
+            });
+          })
+        );
+        this.pokemons.sort((a, b) => a.id - b.id);
+        this.pokemonsFilters = [...this.pokemons]
+      } catch (error) {
+        console.error('Error fetching Pokémon data:', error);
+      }
+    },
+    capitalizarPrimeraLetra(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+  },
 };
 </script>
 
 <style>
 .titulo {
-       color: #ffcb05;
-       text-shadow: -1px -1px 0 #007acc, 1px -1px 0 #007acc, -1px 1px 0 #007acc, 1px 1px 0 #007acc;
-       text-align: center;
-       font-size: 50px;
-   }
-
+  color: #ffcb05;
+  text-shadow: -1px -1px 0 #007acc, 1px -1px 0 #007acc, -1px 1px 0 #007acc, 1px 1px 0 #007acc;
+  text-align: center;
+  font-size: 50px;
+}
 </style>
