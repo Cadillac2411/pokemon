a0e350b5339005c4c7b7650fd7e07a91fefc3352
Baruc <baruc.cx@gmail.com>
2024-05-02 14:37:08 -0600

    Agregando vuetify

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -7,7 +7,33 @@
       <h3 class="nombre">{{ pokemon.name }}</h3>
       <p><b class="id">ID:</b> {{ pokemon.id }}</p>
       <p><b class="tipo">Tipo: </b>{{ pokemon.types.join(', ') }}</p>
-      
+      <v-dialog max-width="500">
+  <template v-slot:activator="{ props: activatorProps }">
+    <v-btn
+      v-bind="activatorProps"
+      color="surface-variant"
+      text="Open Dialog"
+      variant="flat"
+    ></v-btn>
+  </template>
+
+  <template v-slot:default="{ isActive }">
+    <v-card title="Dialog">
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="Close Dialog"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
     </div>
   </div>
 </template>
