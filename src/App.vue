e04660aab8ccbfbad30ae2c728e5b9bed6e48252
SergioCruzSalinas <sergiocruz7913@gamil.com>
2024-05-02 13:47:55 -0600

    se agregan componentes

--- src/App.vue
+++ src/App.vue
@@ -1,128 +1,89 @@
 <template>
-    <div>
-      <h1 class="titulo">Pokémon</h1>
-      <div class="filtro">
-        <input v-model="searchTerm" placeholder="Buscar por nombre o ID" />
-        <div class="pokemon-layout">
-          <div
-            v-for="pokemon in filteredPokemons"
-            :key="pokemon.name"
-            class="pokemon-card"
-          >
-            <div class="imagen">
-              <img :src="pokemon.img" :alt="`Imagen de ${pokemon.name}`" />
-            </div>
-            <h3 class="nombre">{{ pokemon.name }}</h3>
-            <p><b class="id">ID:</b> {{ pokemon.id }}</p>
-            <p><b class="tipo">Tipo: </b>{{ pokemon.types.join(", ") }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  import axios from "axios";
-  
-  export default {
-    data() {
-      return {
-        pokemons: [],
-        searchTerm: "",
-      };
+  <div>
+    <h1 class="titulo">Pokémon</h1>
+  </div>
+
+  <div>
+    <PokemonFilter @update-search-term="handleUpdateSearchTerm" />
+  </div>
+
+  <div>
+    <PokemonList :pokemons="pokemons" :filteredPokemons="pokemonsFilters" />
+  </div>
+</template>
+
+<script>
+import PokemonFilter from '/src/components/PokemonFilter.vue';
+import PokemonList from '/src/components/PokemonList.vue';
+import axios from 'axios';
+
+export default {
+  components: {
+    PokemonList,
+    PokemonFilter
+  },
+  data() {
+    return {
+      pokemons: [],
+      searchTerm:'',
+      pokemonsFilters: []
+    };
+  },
+  created() {
+    this.fetchAllPokemons();
+  },
+  methods: {
+    filteredPokemons(filterTerm) {
+      if (!filterTerm) {
+        this.pokemonsFilters = [...this.pokemons];
+        return;
+      }
+
+      filterTerm = filterTerm.toLowerCase();
+
+      let pokemonsWithFilter = this.pokemons.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(filterTerm) ||
+        parseInt(filterTerm) === pokemon.id
+      );
+
+      this.pokemonsFilters = [...pokemonsWithFilter];
     },
-    computed: {
-      filteredPokemons() {
-        return this.pokemons.filter(pokemon =>
-        pokemon.id === parseInt(this.searchTerm)||
-        pokemon.name.toLowerCase().includes(this.searchTerm.toLowerCase())
-      )},
+    handleUpdateSearchTerm(filterTerm) {
+      this.filteredPokemons(filterTerm);
     },
-    created() {
-      this.fetchAllPokemons();
+    async fetchAllPokemons() {
+      try {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=811');
+        const pokemonList = response.data.results;
+        await Promise.all(
+          pokemonList.map(async (pokemon) => {
+            const pokemonResponse = await axios.get(pokemon.url);
+            this.pokemons.push({
+              name: this.capitalizarPrimeraLetra(pokemonResponse.data.name),
+              id: pokemonResponse.data.id,
+              types: pokemonResponse.data.types.map((typeInfo) => typeInfo.type.name),
+              img: pokemonResponse.data.sprites.front_default,
+            });
+          })
+        );
+        this.pokemons.sort((a, b) => a.id - b.id);
+        this.pokemonsFilters = [...this.pokemons]
+      } catch (error) {
+        console.error('Error fetching Pokémon data:', error);
+      }
     },
-    methods: {
-      async fetchAllPokemons() {
-        try {
-          const response = await axios.get(
-            "https://pokeapi.co/api/v2/pokemon/?limit=811"
-          );
-          const pokemonList = response.data.results;
-          await Promise.all(
-            pokemonList.map(async (pokemon) => {
-              const pokemonResponse = await axios.get(pokemon.url);
-              this.pokemons.push({
-                name: this.capitalizarPrimeraLetra(pokemonResponse.data.name),
-                id: pokemonResponse.data.id,
-                types: pokemonResponse.data.types.map(
-                  (typeInfo) => typeInfo.type.name
-                ),
-                img: pokemonResponse.data.sprites.front_default,
-              });
-            })
-          );
-          // Ordenar los Pokémon por ID de menor a mayor
-          this.pokemons.sort((a, b) => a.id - b.id);
-        } catch (error) {
-          console.error("Error fetching Pokémon data:", error);
-        }
-      },
-      capitalizarPrimeraLetra(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-      },
+    capitalizarPrimeraLetra(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
     },
-  };
-  </script>
-  
-  
+  },
+};
+</script>
 
 <style>
-  * {
-    background-color: white;
-  }
-
-  input {
-    text-align: center;
-  }
-
-  .pokemon-layout {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-  }
-  .pokemon-card {
-    border: 2px solid #ffcb05;
-    padding: 20px;
-    margin: 10px;
-    width: 200px;
-    border-radius: 10px;
-    color: #000;
-  }
-
-  .imagen {
-    background-color: #000;
-    border-radius: 10px;
-  }
-  img {
-    background-color: #000;
-  }
-
-  .titulo {
-    color: #ffcb05;
-    text-shadow: -1px -1px 0 #007acc, 1px -1px 0 #007acc, -1px 1px 0 #007acc,
-      1px 1px 0 #007acc;
-    text-align: center;
-  }
-
-  .nombre {
-    color: #ffcb05;
-    text-shadow: -1px -1px 0 #007acc, 1px -1px 0 #007acc, -1px 1px 0 #007acc,
-      1px 1px 0 #007acc;
-  }
-
-  .filtro {
-    color: #000;
-    text-align: center;
-    font-family: "Times New Roman", Times, serif;
-  }
+.titulo {
+  color: #ffcb05;
+  text-shadow: -1px -1px 0 #007acc, 1px -1px 0 #007acc, -1px 1px 0 #007acc, 1px 1px 0 #007acc;
+  text-align: center;
+  font-size: 50px;
+}
 </style>
---
+++ src/components/PokemonList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="pokemon-layout">
+    <div v-for="pokemon in filteredPokemons" :key="pokemon.name" class="pokemon-card">
+      <div class="imagen">
+        <img :src="pokemon.img" :alt="`Imagen de ${pokemon.name}`" />
+      </div>
+      <h3 class="nombre">{{ pokemon.name }}</h3>
+      <p><b class="id">ID:</b> {{ pokemon.id }}</p>
+      <p><b class="tipo">Tipo: </b>{{ pokemon.types.join(', ') }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemons: {
+      type: Array,
+      required: true
+    },
+    filteredPokemons: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.pokemon-layout {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.pokemon-card {
+  border: 2px solid #ffcb05;
+  padding: 20px;
+  margin: 10px;
+  width: 400px;
+  height: 350px;
+  border-radius: 10px;
+  color: #000;
+}
+.imagen {
+  background-color: #000;
+  border-radius: 10px;
+}
+img {
+  background-color: #000;
+  width: 200px;
+  height: 150px;
+}
+.nombre {
+  color: #ffcb05;
+  text-shadow: -1px -1px 0 #007acc, 1px -1px 0 #007acc, -1px 1px 0 #007acc, 1px 1px 0 #007acc;
+}
+</style>
